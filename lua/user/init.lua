0a7f0937d213bd290cd4babbef2f5d0bb563746e
MikhTrush <uselessmash@gmail.com>
2025-09-30 13:56:33 +0700

    Added lazy.nvim Added mini.pairs plugin

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -4,3 +4,23 @@ require("user.autocmds")
 require("user.usercmds")
 require("user.packages")
 require("user.utils")
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system {
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  { import = "user.plugins" },
+},
+{
+  change_detection = { enabled = false },
+})
---
+++ lua/user/plugins/mini.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        'echasnovski/mini.pairs',
+        opts = {},
+        config = function(_, opts)
+            require('mini.pairs').setup(opts)
+        end,
+    },
+}
