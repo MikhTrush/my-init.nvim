a8fcf8b1a0703b4f8d795e441c563249b4f0bbd9
MikhTrush <uselessmash@gmail.com>
2025-10-27 22:17:28 +0700

    added vscode theme

--- lua/user/plugins/theme.lua
+++ lua/user/plugins/theme.lua
@@ -1,3 +1,12 @@
 return {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+        'Mofiqul/vscode.nvim',
+        config = function()
+            vim.o.termguicolors = true
+            vim.cmd [[colorscheme vscode]]
+            -- Можно выбрать variant:
+            vim.g.vscode_style = "dark"     -- "light" или "dark"
+        end,
+    }
 }
