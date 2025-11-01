7c05670523609735c93bd5e330d9a2ea2717ed89
MikhTrush <uselessmash@gmail.com>
2025-11-01 11:34:58 +0700

    Snippets

--- lua/user/plugins/blink.lua
+++ lua/user/plugins/blink.lua
@@ -13,7 +13,7 @@ return
                 -- This step is not supported in many windows environments.
                 -- Remove the below condition to re-enable on windows.
                 -- if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-                    -- return
+                -- return
                 -- end
                 return 'make install_jsregexp'
             end)(),
@@ -21,12 +21,12 @@ return
                 -- `friendly-snippets` contains a variety of premade snippets.
                 --    See the README about individual language/framework/plugin snippets:
                 --    https://github.com/rafamadriz/friendly-snippets
-                -- {
-                --   'rafamadriz/friendly-snippets',
-                --   config = function()
-                --     require('luasnip.loaders.from_vscode').lazy_load()
-                --   end,
-                -- },
+                {
+                    'rafamadriz/friendly-snippets',
+                    config = function()
+                        require('luasnip.loaders.from_vscode').lazy_load()
+                    end,
+                },
             },
             opts = {},
         },
