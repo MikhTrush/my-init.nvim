3a19418a18aa0c9a6975f1cd9ac2fd9f68f9cbde
MikhTrush <uselessmash@gmail.com>
2025-11-19 16:34:05 +0700

    Changed default shell to pwsh and added toggleterm

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,11 +1,12 @@
 local o = vim.o
 
---[[ Line numbering ]] --
+--[[ Line numbering ]]
+--
 vim.o.number = true
 vim.o.relativenumber = true
 
 -- Enable mouse mode
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
@@ -15,30 +16,35 @@ vim.o.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+	vim.o.clipboard = "unnamedplus"
 end)
 
---[[ Search settings ]] --
+--[[ Search settings ]]
+--
 vim.o.ignorecase = true
-vim.o.smartcase = true      -- Considers case, if search text has Uppercase
-vim.o.showmatch = true      -- Shows results
+vim.o.smartcase = true -- Considers case, if search text has Uppercase
+vim.o.showmatch = true -- Shows results
 
---[[ Tab settings ]]    --
+--[[ Tab settings ]]
+--
 vim.o.shiftwidth = 4
 vim.o.tabstop = 4
 vim.o.smartindent = true
 -- vim.o.expandtab = false
 
---[[ Split ]] --
+--[[ Split ]]
+--
 vim.o.splitright = true
 vim.o.splitbelow = true
 
-vim.o.ve = all
+-- vim.o.ve =
 
---[[ Files ]] --
-vim.o.completeopt = 'menuone,noselect'
+--[[ Files ]]
+--
+vim.o.completeopt = "menuone,noselect"
 
---[[ Appearance settings ]] --
+--[[ Appearance settings ]]
+--
 -- Highlight the line where the cursor is on
 vim.o.cursorline = true
 
@@ -47,19 +53,18 @@ vim.o.scrolloff = 5
 
 -- Show <tab> and trailing spaces
 vim.o.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s) See `:help 'confirm'`
 vim.o.confirm = true
-
 
 -- Don't know what these do
 -- Enable break indent
 vim.o.breakindent = true
 
 -- Keep signcolumn on by default
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 
 -- Decrease update time
 -- vim.o.updatetime = 250
@@ -68,7 +73,10 @@ vim.o.signcolumn = 'yes'
 vim.o.timeoutlen = 300
 
 -- Preview substitutions live, as you type!
-vim.o.inccommand = 'split'
+vim.o.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.o.cursorline = true
+
+vim.o.shell = "pwsh"
+
---
+++ lua/user/plugins/toggleterm.lua
@@ -0,0 +1,6 @@
+return {
+	-- amongst your other plugins
+	{ "akinsho/toggleterm.nvim", version = "*", config = true, opts = {} },
+	-- or
+	-- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
+}
